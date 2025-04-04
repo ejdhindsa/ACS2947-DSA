d0ca542715345167760c20e985e22718cb6e5f7d
ejdhindsa <ejdhindsa@Gmail.com>
2025-04-04 14:29:30 -0500

    make quick sort

---
+++ src/AssignmentFour/QuickSort.java
@@ -0,0 +1,56 @@
+package AssignmentFour;
+
+import java.util.Comparator;
+
+public class QuickSort
+{
+    /**
+     * Quick-sort contents of a Queue
+     * @param S Array S which is to be sorted
+     * @param comp comparator with which the queue is sorted
+     * @param <K> Generic type K to which the elements belong
+     */
+    public static <K> void quickSortInPlace(K[] S, Comparator<K> comp, int a, int b)
+    {
+        if (a >= b)
+            return;                 // array is trivially sorted
+
+        int left = a;
+        int right = b - 1;
+        K pivot = S[b];
+        K temp;                     // temp object used for swapping
+
+        while (left <= right)
+        {
+            // scan until reaching value equal or larger than pivot (or right marker)
+            while (left <= right && comp.compare(S[left], pivot) < 0)
+                left++;
+
+            // scan until reaching value equal or smaller than pivot (or left markeer)
+            while (left <= right && comp.compare(S[right], pivot) > 0)
+                right--;
+
+            if (left <= right)          // indices did not strictly cross
+            {
+                // so swap values and shrink range
+                temp = S[left];
+                S[left] = S[right];
+                S[right] = temp;
+                left++;
+                right--;
+            } // end of if
+
+        } // end of while
+
+        // put pivot into its final place (currently marked by left index)
+        temp = S[left];;
+        S[left] = S[b];
+        S[b] = temp;
+
+        // make recursive calls
+        quickSortInPlace(S, comp, a, left - 1);
+        quickSortInPlace(S, comp, left + 1, b);
+
+    } // end of quickSort()
+
+} // end of class
